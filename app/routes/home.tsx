afdac1938f47a2d5d32bc08e119ef333efe10448
Kamal <kamalkdk15@gmail.com>
2025-09-11 20:17:12 +0530

    nav home & resumecard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResuMind" },
+    { name: "description", content: "AI that understands your resume" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+<section className="main-section">
+<div className="page-heading py-15">
+<h1>
+  Smart Resume Insights & Application Tracking
+</h1>
+<h2>
+  Track Your Submissions. Improve with AI Feedback.
+</h2>
+</div>
+{Array.isArray(resumes) && resumes.length > 0 && (
+  <div className="resumes-section">
+    {resumes.map((resume) => (
+      <div key={resume.id}>
+        <ResumeCard resume={resume} />
+      </div>
+    ))}
+  </div>
+)}
+</section>
+  </main>
 }
