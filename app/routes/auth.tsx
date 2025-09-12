55c4d06f4b2d8d2358dcb3ded19c0a229b7c435a
Kamal <kamalkdk15@gmail.com>
2025-09-12 18:16:19 +0530

    Authentication

---
+++ app/routes/auth.tsx
@@ -0,0 +1,57 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+
+export const meta = () => (
+    [
+        {title: 'ResuMind | Auth'},
+        {name: 'description', content: 'Authentication Page '},
+
+    ]
+)
+
+
+const Auth = () => {
+    
+const {isLoading, auth} = usePuterStore();
+const location = useLocation();
+const next = location.search.split('next=')[1];
+const navigate = useNavigate();
+
+useEffect(() => {
+if(auth.isAuthenticated) navigate(next);
+}, [auth.isAuthenticated, next]);
+
+    return (
+         <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+<div className="gradient-border shadow-lg">
+<section className="flex flex-col gap-8 bg-white rounded-2xl p-10 ">
+<div className="items-center flex flex-col gap-2 text-center">
+    <h1>Welcome</h1>
+    <h2>Log in to continue your Job journey</h2>
+</div>
+{isLoading ? (
+    <button className="auth-button animated-pulse" >
+       <p>Signing In...</p> 
+    </button>
+): (
+    <>
+    {auth.isAuthenticated ? (
+        <button className="auth-button" onClick={auth.signOut}>
+           <p>Log Out</p>
+        </button>
+    ): (
+        <button className="auth-button" onClick={auth.signIn}>
+         <p> Log in</p>
+        </button>
+    )}
+    </>
+)}
+</section>
+</div>
+        </main>
+    )
+    
+};
+
+export default Auth;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,11 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-
 import ResumeCard from "~/components/ResumeCard";
 import { resumes } from "constants";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,7 +15,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+  const {auth} = usePuterStore();
+
+const navigate = useNavigate();
+
+useEffect(() => {
+if(!auth.isAuthenticated) navigate('/auth?next=/');
+}, [auth.isAuthenticated]);
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen">
     <Navbar/>
 <section className="main-section">
 <div className="page-heading py-15">
